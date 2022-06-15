a027db38c8581a37dc3150180c41d65bbab9b7ac
sh.choi_MAC <csonghui339@gmail.com>
2022-06-15 11:50:39 +0900

    220615_3교시_데이터입출력

--- 03_JavaScript/01_개요.js
+++ 03_JavaScript/01_개요.js
@@ -14,3 +14,12 @@ var btn2 = document.getElementById('btn2');
 // 함수를 호출하는 구문 + ()
 // btn2.onclick = consoleTest();
 btn2.onclick = consoleTest;
+
+// 데이터 출력
+// function checkConfirm() {
+//     if(confirm('여러분 배고픈가요?')) {
+//         alert('저도 배고파요~');
+//     } else {
+//         alert('저만 배고픈가요?ㅜㅅㅠ');
+//     }
+// }
