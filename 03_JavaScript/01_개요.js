ef13e2d4740cdf2c8dea6090b976836ea38f3e19
sh.choi_MAC <csonghui339@gmail.com>
2022-06-16 09:49:29 +0900

    220616_1교시_복습 및 text필드의 작성된 값 읽어오기

--- 03_JavaScript/01_개요.js
+++ 03_JavaScript/01_개요.js
@@ -2,18 +2,18 @@
     01_개요 JavaScript
 */
 
+
+// 변수 선언
+var btn2 = document.getElementById('btn2');
+// 함수를 호출하는 구문 + ()
+// btn2.onclick = consoleTest();
+btn2.onclick = consoleTest;
+
 // 자바의 method
 // Function은 반환형이 없음
 function consoleTest() {
     console.log('버튼 클릭!');
 }
-
-// 변수 선언
-var btn2 = document.getElementById('btn2');
-
-// 함수를 호출하는 구문 + ()
-// btn2.onclick = consoleTest();
-btn2.onclick = consoleTest;
 
 // 데이터 출력
 // function checkConfirm() {
