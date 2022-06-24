c934525ee13b2608580ca9edef601e5f37e5e9a3
sh.choi_MAC <csonghui339@gmail.com>
2022-06-24 12:40:03 +0900

    220624_4교시_객체조작(요소생성)

--- 04_jQuery/07_객체조작.js
+++ 04_jQuery/07_객체조작.js
@@ -27,4 +27,43 @@ $(document).ready(function() {
         return `<h1>인덱스 : ${index}, 내용 : ${content}</h1>`;
     });
 
+    // 2. 요소 추가
+    // 1) 요소 생성
+    // 문자열로 요소를 생성하는 방법
+    let p1 = '<p>문자열로 요소를 생성</p>';
+
+    // 제이쿼리로 요소를 생성하는 방법
+    let p2 = $('<p>').text('제이쿼리로 요소를 생성1');
+    let p3 = $(`<p>제이쿼리로 요소를 생성2</p>`);
+
+    $('#create').append(p1, p2, p3);
+
+    // 2) 요소 추가 #1
+    // $(A).append(B) : A 요소의 자식 요소로 B 요소를 뒷 부분에 추가한다 
+    $('#add1').append('<span>B</span>')
+    // $(A).prepend(B) : A 요소의 자식 요소로 B 요소를 앞 부분에 추가한다 
+    $('#add2').prepend('<span>B</span>')
+    // $(A).after(B) : A 요소의 형제 요소로 B 요소를 뒷 부분에 추가한다 
+    $('#add3').after('<span>B</span>')
+    // $(A).before(B) : A 요소의 형제 요소로 B 요소를 앞 부분에 추가한다 
+    $('#add4').before('<span>B</span>')
+    
+    // 3) 요소 추가 #2
+    // $(A).appendTo(B) : A 요소를 B 요소의 자식 요소로 뒷 부분에 추가한다
+    $('<span>B</span>').appendTo('#add5');
+    // $(A).prependTo(B) : A 요소를 B 요소의 자식 요소로 앞 부분에 추가한다
+    $('<span>B</span>').prependTo('#add6');
+    // $(A).insertAfter(B) : A 요소를 B 요소의 형제 요소로 뒷 부분에 추가한다
+    $('<span>B</span>').insertAfter('#add7');
+    // $(A).insertBefore(B) : A 요소를 B 요소의 형제 요소로 앞 부분에 추가한다
+    $('<span>B</span>').insertBefore('#add8');
+
+
+
+
+
+
+
+
+
 });
